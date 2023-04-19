52b5dc504952c64418aca97694e95dfc0592a78b
wee-owl <kem-cv@yandex.ru>
2023-04-19 12:22:05 +0300

    feat: publish project

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'), // каталог для результатов сборки 
+    filename: 'index.js', // имя файла с результатами сборки (как в entry point)
+    library: 'ajs', // название библиотеки
+    libraryTarget: 'umd', // UMD (Universal Module Definition https://github.com/umdjs/umd) - шаблон, который позволяет использовать RequireJS и браузер
+    libraryExport: 'default', // экспортируется имя default
+    globalObject: 'this', // что принимать за глобальный объект, иначе сгенерируется window, которого на платформе Node.js нет
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
